fd06686dc0bc634f7bfd90882dce246e2557067b
5horty <roganemacleod@gmail.com>
2026-02-24 20:36:14 +0000

    minor change

--- frontend/lib/pages/login.dart
+++ frontend/lib/pages/login.dart
@@ -91,7 +91,7 @@ class _LoginPageState extends State<LoginPage> {
                                     );
                                 }, 
                                 child: 
-                                    Text("LOgin"),
+                                    Text("Login"),
                             )
                         ],
                     )
