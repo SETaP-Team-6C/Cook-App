0c31ab3758cd533c42a6323727016f12e32170c8
aidanjohnys <aidan.johnys@gmail.com>
2026-03-01 16:30:21 +0000

    add additional tests

--- backend/test/authentication.py
+++ backend/test/authentication.py
@@ -15,8 +15,7 @@ def test_successful(client: FlaskClient) -> None:
     assert response.json["user"]["user_id"] == 1
 
 
-def test_bad_username(client: FlaskClient) -> None:
-    # Test bad username
+def test_incorrect_fname(client: FlaskClient) -> None:
     body = {
         "user_fname": "fake",
         "user_lname": "user",
@@ -27,8 +26,18 @@ def test_bad_username(client: FlaskClient) -> None:
     assert response.json is None
 
 
-def test_bad_fname(client: FlaskClient) -> None:
-    # Test missing first name
+def test_incorrect_lname(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "test",
+        "user_lname": "person",
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 401
+    assert response.json is None
+
+
+def test_missing_fname(client: FlaskClient) -> None:
     body = {
         "user_lname": "user",
     }
@@ -38,10 +47,51 @@ def test_bad_fname(client: FlaskClient) -> None:
     assert response.json is None
 
 
-def test_bad_lname(client: FlaskClient) -> None:
-    # Test missing last name
+def test_missing_lname(client: FlaskClient) -> None:
     body = {
         "user_fname": "test"
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 400
+    assert response.json is None
+
+
+def test_blank_fname(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "",
+        "user_lname": "user",
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 401
+    assert response.json is None
+
+
+def test_blank_lname(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "test",
+        "user_lname": ""
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 401
+    assert response.json is None
+
+
+def test_empty_inputs(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "",
+        "user_lname": ""
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 401
+    assert response.json is None
+
+
+def test_empty_body(client: FlaskClient) -> None:
+    body = {
     }
 
     response = client.post("/authenticate", data=body)
