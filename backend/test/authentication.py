d60459cc5a995fd05d72511a0f5f3aeb1558446e
aidanjohnys <aidan.johnys@gmail.com>
2026-03-01 16:23:50 +0000

    add additional tests for authentication

--- backend/test/authentication.py
+++ backend/test/authentication.py
@@ -2,16 +2,48 @@ from flask.testing import FlaskClient
 
 from conftest import client
 
-def test_authentication(client: FlaskClient) -> None:
+
+def test_successful(client: FlaskClient) -> None:
+    # Test successful authentication
     body = {
         "user_fname": "test",
         "user_lname": "user",
     }
 
     response = client.post("/authenticate", data=body)
-
     assert response.status_code == 200
     assert response.json["user"]["user_id"] == 1
 
 
+def test_bad_username(client: FlaskClient) -> None:
+    # Test bad username
+    body = {
+        "user_fname": "fake",
+        "user_lname": "user",
+    }
 
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 401
+    assert response.json is None
+
+
+def test_bad_fname(client: FlaskClient) -> None:
+    # Test missing first name
+    body = {
+        "user_lname": "user",
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 400
+    assert response.json is None
+
+
+def test_bad_lname(client: FlaskClient) -> None:
+    # Test missing last name
+    body = {
+        "user_fname": "test"
+    }
+
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 400
+    assert response.json is None
