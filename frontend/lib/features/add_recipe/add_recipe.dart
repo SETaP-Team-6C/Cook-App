59454ee1a5bd83abe4dc617029f0a9d5b4a805a0
5horty <roganemacleod@gmail.com>
2026-03-06 15:43:13 +0000

    added recipe time, difficulty and reworked ingrediants to be a class sot that name amoutn and calorie info can be stored together

--- frontend/lib/features/add_recipe/add_recipe.dart
+++ frontend/lib/features/add_recipe/add_recipe.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+// todo: add recipe time, recipe calories
+// future version will also need dietary requirements
+class Ingredient {
+  final TextEditingController name = TextEditingController();
+  final TextEditingController amount = TextEditingController();
+  final TextEditingController calories = TextEditingController();
+}
+
 class AddRecipe extends StatefulWidget {
   const AddRecipe({super.key});
 
@@ -11,27 +19,35 @@ class AddRecipe extends StatefulWidget {
 
 class _AddRecipeState extends State<AddRecipe> {
   final TextEditingController _recipeNameController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  
   final _formKey = GlobalKey<FormState>();
 
-  final List<TextEditingController> _ingredientsController = [];
   final List<TextEditingController> _stepsController = [];
+  final List<Ingredient> _ingredients = [];
+
+  String? _difficultySelector;
 
   @override
   void initState() {
     super.initState();
 
     for (int i = 0; i < 3; i++) {
-      _ingredientsController.add(TextEditingController());
       _stepsController.add(TextEditingController());
+      _ingredients.add(Ingredient());
+
     }
   }
 
   @override
   void dispose() {
     _recipeNameController.dispose();
+    _timeController.dispose();
 
-    for (var cont in _ingredientsController) {
-      cont.dispose();
+    for (var cont in _ingredients) {
+      cont.name.dispose();
+      cont.amount.dispose();
+      cont.calories.dispose();
     }
 
     for (var cont in _stepsController) {
@@ -40,17 +56,19 @@ class _AddRecipeState extends State<AddRecipe> {
 
     super.dispose();
   }
-Future<void> _sendRecipe(name, ingredients , steps) async {
+Future<void> _sendRecipe(name, ingredients , steps, time, difficulty) async {
     try {
         final response = await http.post(
-            Uri.parse("http://localhost:5000/recipe"),  
+            Uri.parse("http://localhost:5000/add-recipe"),  
             headers: {
                 "Content-Type": "application/json", 
             },
             body: jsonEncode({ 
-                "recipe_name": name,
-                "ingredients": ingredients,
-                "steps": steps
+                "recipe-title": name,
+                "recipe-ingredients": ingredients,
+                "recipe-steps": steps,
+                "recipe-time": time,
+                "recipe-difficulty": difficulty,
             }),
         );
             print("got in func");
@@ -68,25 +86,21 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
   Future<void> _saveRecipe() async {
     if (_formKey.currentState!.validate()) {
       final name = _recipeNameController.text.trim();
+      final time = _timeController.text.trim();
+      final difficulty = _difficultySelector;
 
-      final ingredients = _ingredientsController
-          .map((c) => c.text.trim())
-          .where((text) => text.isNotEmpty)
-          .toList();
+      final ingredients = _ingredients.map((ingredient){
+        return{
+            "ingredient-name":ingredient.name.text.trim(),
+            "ingredient-amount": ingredient.amount.text.trim(),
+            "ingredient-calories": ingredient.calories.text.trim(),
+        };
+      }).toList();
 
       final steps = _stepsController
           .map((c) => c.text.trim())
           .where((text) => text.isNotEmpty)
           .toList();
-
-      if (ingredients.isEmpty) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text("Please add at least one ingredient"),
-          ),
-        );
-        return;
-      }
 
       if (steps.isEmpty) {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -96,11 +110,13 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
         );
         return;
       }
-      await _sendRecipe(name, ingredients,steps);
+      await _sendRecipe(name, ingredients,steps,time,difficulty);
 
       print("Name: $name");
       print("Ingredients: $ingredients");
       print("Steps: $steps");
+      print("${time}");
+      print("${difficulty}");
     }
   }
 
@@ -139,6 +155,47 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
               ),
               const SizedBox(height: 20),
 
+              /// Recipe time
+              TextFormField(
+                controller: _timeController,
+                decoration: const InputDecoration(
+                  labelText: "Recipe expected time",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Recipe expected time is required";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              
+              DropdownButtonFormField<String>(
+                initialValue: _difficultySelector,
+                decoration: const InputDecoration(
+                    labelText: "select difficulty",
+                    border: OutlineInputBorder(),
+                ),
+                items: ["easy", "medium","hard"].map((item)=>
+                    DropdownMenuItem(
+                                value:item, 
+                                child:Text(item)
+                  )
+                ).toList(),
+                onChanged: (value){
+                        setState((){
+                            _difficultySelector = value;
+                        });
+                    },
+                validator: (value){
+                        if (value == null || value.isEmpty){
+                            return "please select a difficulty";
+                        }
+                        return null;
+                    }
+              ),
+
               /// Ingredients
               const Text(
                 "Ingredients",
@@ -146,24 +203,76 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
               ),
               const SizedBox(height: 20),
 
-              ..._ingredientsController.asMap().entries.map((entry) {
-                int index = entry.key;
-                TextEditingController controller = entry.value;
+              ..._ingredients.asMap().entries.map((entry) {
+                Ingredient controller = entry.value;
 
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 20),
-                  child: TextFormField(
-                    controller: controller,
-                    decoration: InputDecoration(
-                      labelText: "Ingredient ${index + 1}",
-                      border: const OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "Ingredient cannot be empty";
-                      }
-                      return null;
-                    },
+                  child: Row(
+                    children: [
+                        Expanded(
+                            flex: 3,
+                            child: TextFormField(
+                                controller: controller.name,
+                                decoration: const InputDecoration(
+                                    labelText: "Ingredient name",
+                                    border: OutlineInputBorder(),
+                          ),
+                            validator: (value){
+                                if (value == null || value.trim().isEmpty){
+                                    return "enter a valid name";
+                                }
+                                return null;
+                            },
+                        ),
+                      ),
+
+                       const SizedBox(height: 20),
+
+                       Expanded(
+                        flex: 2,
+                        child: TextFormField(
+                            controller: controller.amount,
+                            decoration: const InputDecoration(
+                                labelText: "Ingredient amount",
+                                border: OutlineInputBorder(),
+                          ),
+                            validator: (value){
+                                if (value == null || value.trim().isEmpty){
+                                    return "enter a valid amount";
+                                }
+                                return null;
+                            },
+                        ),
+                      ),
+
+                       const SizedBox(height: 20),
+
+                       Expanded(
+                        flex: 2,
+                        child: TextFormField(
+                            controller: controller.calories,
+                            decoration: const InputDecoration(
+                                labelText: "Ingredient calories",
+                                border: OutlineInputBorder(),
+                          ),
+                            validator: (value){
+                                if (value == null || value.trim().isEmpty){
+                                    return "enter a valid amount";
+                                }
+                                final calories = int.tryParse(value);
+                                if (calories == null){
+                                    return "must be a number ";
+                                }
+                                if (calories < 0){
+                                    return "must be a positve number";
+                                }
+                                return null;
+                            },
+                          keyboardType: TextInputType.number,
+                        ),
+                      ),
+                    ],
                   ),
                 );
               }),
@@ -171,7 +280,7 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
               ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    _ingredientsController.add(TextEditingController());
+                     _ingredients.add(Ingredient());
                   });
                 },
                 child: const Text("Add Ingredient"),
@@ -194,7 +303,6 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
                   padding: const EdgeInsets.only(bottom: 20),
                   child: TextFormField(
                     controller: controller,
-                    maxLines: 3,
                     decoration: InputDecoration(
                       labelText: "Step ${index + 1}",
                       border: const OutlineInputBorder(),
@@ -202,9 +310,6 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
                     validator: (value) {
                       if (value == null || value.trim().isEmpty) {
                         return "Step cannot be empty";
-                      }
-                      if (value.trim().length < 5) {
-                        return "Step must be more descriptive";
                       }
                       return null;
                     },
@@ -220,7 +325,7 @@ Future<void> _sendRecipe(name, ingredients , steps) async {
                 },
                 child: const Text("Add Step"),
               ),
-
+              
               const SizedBox(height: 30),
 
               Center(
