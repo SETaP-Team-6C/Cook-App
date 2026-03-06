fffd3670847036ae512c861c000da2156d42b174
aidanjohnys <aidan.johnys@gmail.com>
2026-03-06 12:01:04 +0000

    add new route

--- backend/main/recipe/routes.py
+++ backend/main/recipe/routes.py
@@ -1,9 +1,12 @@
 from flask import blueprints
+from flask import request
+from flask import abort
 
 from main.database import Database
 from main.paths import PROJECT_MAIN
 
 recipe_bp = blueprints.Blueprint('recipes', __name__)
+
 
 @recipe_bp.route('/get-recipes')
 def get_recipes():
@@ -24,3 +27,18 @@ def get_recipes():
         "recipes": recipes
     }
 
+
+@recipe_bp.route('/add-recipe', methods=['POST'])
+def add_recipe():
+    # We are using content-type: application/json for this endpoint!
+    print(request.content_type)
+
+
+    # db = Database()
+    # with db.get_connection() as con:
+    #     cur = con.cursor()
+    #     with open(PROJECT_MAIN / "recipe/sql/add_recipe.sql", 'r') as sql_file:
+    #         sql = sql_file.read()
+    #         cur.execute(sql, (recipe_title, recipe_time, recipe_calories))
+    #
+    #     new_recipe_id = cur.lastrowid
---
+++ backend/main/recipe/sql/add_recipe.sql
@@ -0,0 +1,2 @@
+INSERT INTO recipe (recipe_title, recipe_time, recipe_calories)
+VALUES (?, ?, ?);
