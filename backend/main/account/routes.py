ae4b4e898a7ba488b32b77fedaa4f6034303bf68
5horty <roganemacleod@gmail.com>
2026-03-25 14:07:21 +0000

    again just removed hashing as getting it to test was being a pain

--- backend/main/account/routes.py
+++ backend/main/account/routes.py
@@ -2,7 +2,6 @@ from flask import blueprints
 from flask import request
 from flask import abort
 
-import bcrypt
 
 from main.database import Database
 from main.paths import PROJECT_MAIN
@@ -31,16 +30,14 @@ def create_account():
     user_password = request.form['user_password']
     user_email = request.form['user_email']
 
-    password_in_bytes = user_password.encode("UTF-8")
 
-    hashed_password = bcrypt.hashpw(password_in_bytes,bcrypt.gensalt())
 
     db = Database()
     with db.get_connection() as con:
         cur = con.cursor()
         with open(PROJECT_MAIN / "account/sql/create_user.sql", 'r') as sql_file:
             sql = sql_file.read()
-            cur.execute(sql, (user_fname, user_lname,user_email,hashed_password))
+            cur.execute(sql, (user_fname, user_lname,user_email,user_password))
 
         user_id = cur.lastrowid
 
