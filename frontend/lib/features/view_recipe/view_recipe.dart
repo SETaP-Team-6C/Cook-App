d6b60e308a08f5c5de4b36c9f276adccc8177bb0
5horty <roganemacleod@gmail.com>
2026-04-29 18:55:37 +0100

    niver tempalte will now add http method also moving api class to core

--- frontend/lib/features/view_recipe/view_recipe.dart
+++ frontend/lib/features/view_recipe/view_recipe.dart
@@ -10,19 +10,47 @@ class RecipePage extends StatefulWidget {
 
 class _RecipePageState extends State<RecipePage> {
   // test data
-  final List<Map<String, dynamic>> steps = [
-    {"id": 1, "text": "preheat oven"},
-    {"id": 2, "text": "mix"},
-    {"id": 3, "text": "fix"},
-  ];
+  List<Map<String, dynamic>> steps = [];
   Set<int> completedSteps = {};
 
-  void toggleStep(int stepId) {
+  int? currentStepId;
+
+  @override
+  void initState() {
+    super.initState();
+    loadRecipe();
+  }
+
+  Future<void> loadRecipe() async {
+    // todo make fetch req i will need to make api response gonna put that class in core and use it ther
+    // flake data
+    steps = [
+      {"id": 1, "text": "preheat onven"},
+      {"id": 2, "text": "preheat onven2"},
+      {"id": 3, "text": "preheat onven3"},
+    ];
+    completedSteps = {1};
+
     setState(() {
-      if (completedSteps.contains(stepId)) {
-        completedSteps.remove(stepId);
+      currentStepId = steps.firstWhere(
+        (s) => !completedSteps.contains(s["id"]),
+      )["id"];
+    });
+  }
+
+  Map<String, dynamic> get currentStep =>
+      steps.firstWhere((s) => s["id"] == currentStepId);
+
+  void completedstep() {
+    setState(() {
+      completedSteps.add(currentStepId!);
+      //todo
+      //change db
+      final next = steps.where((s) => !completedSteps.contains(s["id"]));
+      if (next.isEmpty) {
+        currentStepId = null; //complete
       } else {
-        completedSteps.add(stepId);
+        currentStepId = next.first["id"];
       }
     });
   }
@@ -30,40 +58,57 @@ class _RecipePageState extends State<RecipePage> {
   double get progress =>
       steps.isEmpty ? 0 : completedSteps.length / steps.length;
 
+  void goBack() {
+    final completedList = completedSteps.toList();
+    if (completedList.isEmpty) {
+      return;
+    }
+    setState(() {
+      currentStepId = completedList.last;
+      completedSteps.remove(currentStepId);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (currentStepId == null) {
+      return Scaffold(
+        appBar: AppBar(title: const Text("test recipe")),
+        body: const Center(child: Text("completed recipe")),
+      );
+    }
     return Scaffold(
       appBar: AppBar(title: const Text("recipe page test")),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             LinearProgressIndicator(value: progress),
 
             const SizedBox(height: 20),
 
-            Expanded(
-              child: ListView.builder(
-                itemCount: steps.length,
-                itemBuilder: (context, index) {
-                  final step = steps[index];
-                  final isDone = completedSteps.contains(step["id"]);
+            Text("Step ${currentStep["id"]}"),
 
-                  return ListTile(
-                    title: Text(
-                      step["text"], // needs changing for real dsat
-                      style: TextStyle(
-                        decoration: isDone ? TextDecoration.lineThrough : null,
-                      ),
-                    ),
-                    trailing: Checkbox(
-                      value: isDone,
-                      onChanged: (_) => toggleStep(step["id"]),
-                    ),
-                  );
-                },
-              ),
+            const SizedBox(height: 20),
+
+            Text(
+              currentStep["text"],
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20),
             ),
+
+            const Spacer(),
+
+            ElevatedButton(
+              onPressed: completedstep,
+              child: const Text("complete step"),
+            ),
+
+            const SizedBox(height: 20),
+
+            if (completedSteps.isNotEmpty)
+              TextButton(onPressed: goBack, child: const Text("go back")),
           ],
         ),
       ),
