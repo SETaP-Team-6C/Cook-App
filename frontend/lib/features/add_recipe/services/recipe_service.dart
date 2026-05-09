6b3c1f3d04eacbab7b7378cbcd9181b1b86cb50c
5horty <roganemacleod@gmail.com>
2026-05-09 17:02:39 +0100

    got send ready i think will do back now and see

--- frontend/lib/features/add_recipe/services/recipe_service.dart
+++ frontend/lib/features/add_recipe/services/recipe_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class RecipeService {
@@ -8,22 +9,59 @@ class RecipeService {
     List steps,
     String time,
     String difficulty,
+    String? mainImage,
   ) async {
-    final response = await http.post(
-      Uri.parse("http://localhost:5000/add-recipe"),
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({
-        "recipe-title": name,
-        "recipe-ingredients": ingredients,
-        "recipe-steps": steps,
-        "recipe-time": time,
-        "recipe-difficulty": difficulty,
-      }),
-    );
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      return true;
-    } else {
-      throw Exception("faile to add recipe");
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("http://localhost:5000/add-recipe"),
+      );
+
+      // add fields
+      request.fields['recipe-title'] = name;
+      request.fields['recipe-ingredients'] = jsonEncode(ingredients);
+      request.fields['recipe-steps'] = jsonEncode(steps);
+      request.fields['recipe-time'] = time;
+      request.fields['recipe-difficulty'] = difficulty;
+
+      // add main image
+      if (mainImage != null && mainImage.isNotEmpty) {
+        File imageFile = File(mainImage);
+        if (await imageFile.exists()) {
+          request.files.add(
+            await http.MultipartFile.fromPath("recipe-main-image", mainImage),
+          );
+        }
+      }
+
+      // add step images
+      for (var i = 0; i < steps.length; i++) {
+        var step = steps[i];
+        if (step['step-image'] != null && step['step-image'].isNotEmpty) {
+          File stepImageFile = File(step['step-image']);
+          if (await stepImageFile.exists()) {
+            request.files.add(
+              // uses step index to matcht on backend
+              await http.MultipartFile.fromPath(
+                'step-image-${step['step-index']}',
+                step['step-image'],
+              ),
+            );
+          }
+        }
+      }
+      var streamedResponse = await request.send();
+      var response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return true;
+      } else {
+        throw Exception(
+          "Failed to add recipe: ${response.statusCode} - ${response.body}",
+        );
+      }
+    } catch (e) {
+      throw Exception("failed to add recipe : $e");
     }
   }
 }
