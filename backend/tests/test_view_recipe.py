107a9e62c33471bbfe48b1f03de7e8a2f65ae2a2
aidanjohnys <aidan.johnys@gmail.com>
2026-05-12 12:44:38 +0100

    add image test

--- backend/tests/test_view_recipe.py
+++ backend/tests/test_view_recipe.py
@@ -1,5 +1,7 @@
 import json
 
+from werkzeug.datastructures import FileStorage
+
 
 def test_view_recipe(client):
     body = {
@@ -87,4 +89,64 @@ def test_view_recipe(client):
             "recipe-time": "PT1H30M"
         })
 
-# todo: more tests...
+def test_recipe_image(client):
+    body = {
+        "user_fname": "test",
+        "user_lname": "user",
+        "user_password": "123456"
+    }
+    response = client.post("/authenticate", data=body)
+    assert response.status_code == 200
+
+    image = FileStorage(
+        stream=open("assets/test_image.jpg", "rb"),
+        filename="test_image.jpg",
+        content_type="image/jpeg"
+    )
+
+    body = {
+        "recipe-main-image": image,
+        "recipe-ingredients": json.dumps([
+            {
+                "ingredient-name": "Ingredient 1",
+                "ingredient-amount": 300,
+                "ingredient-unit": "g",
+                "ingredient-calories": 500
+            },
+            {
+                "ingredient-name": "Ingredient 2",
+                "ingredient-amount": 400,
+                "ingredient-unit": "g",
+                "ingredient-calories": 700
+            }
+        ]),
+        "recipe-title": "Test Recipe",
+        "recipe-steps": json.dumps([
+            {
+                "step-description": "Step 1",
+                "step-duration": "PT30M",
+                "step-index": "1"
+            },
+            {
+                "step-description": "Step 2",
+                "step-duration": "PT45M",
+                "step-index": "2"
+            }
+        ]),
+        "recipe-difficulty": "medium",
+        "recipe-time": "PT1H30M"
+    }
+    response = client.post('/add-recipe', data=body, content_type="multipart/form-data")
+    assert response.status_code == 201
+    assert response.mimetype == "application/json"
+    recipe_id = response.get_json()["recipe-id"]
+
+    image = FileStorage(
+        stream=open("assets/test_image.jpg", "rb"),
+        filename="test_image.jpg",
+        content_type="image/jpeg"
+    )
+    response = client.get(f"/recipe-image/{recipe_id}")
+    assert response.status_code == 200
+    assert response.data == image.stream.read()
+    assert response.mimetype == "image/jpeg"
