ec5ccfeaf12349ef89ce4b5965c8f36408031295
aidanjohnys <aidan.johnys@gmail.com>
2026-05-15 15:14:26 +0100

    add blank tests for account

--- backend/tests/test_account.py
+++ backend/tests/test_account.py
@@ -57,3 +57,51 @@ def test_account_creation_missing_email(client: FlaskClient) -> None:
 
     response = client.post('/create-account', data=body)
     assert response.status_code == 400
+
+
+def test_account_blank_fname(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "",
+        "user_lname": "user",
+        "user_email": "test@test",
+        "user_password": "123456"
+    }
+
+    response = client.post('/create-account', data=body)
+    assert response.status_code == 400
+
+
+def test_account_blank_lname(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "new",
+        "user_lname": "",
+        "user_email": "test@test",
+        "user_password": "123456"
+    }
+
+    response = client.post('/create-account', data=body)
+    assert response.status_code == 400
+
+
+def test_account_blank_email(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "new",
+        "user_lname": "user",
+        "user_email": "test@test",
+        "user_password": "123456"
+    }
+
+    response = client.post('/create-account', data=body)
+    assert response.status_code == 400
+
+
+def test_account_blank_password(client: FlaskClient) -> None:
+    body = {
+        "user_fname": "new",
+        "user_lname": "user",
+        "user_email": "test@test",
+        "user_password": "123456"
+    }
+
+    response = client.post('/create-account', data=body)
+    assert response.status_code == 400
